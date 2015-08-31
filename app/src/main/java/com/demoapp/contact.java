f6f12bda5362dcdc524e3129547944a388b16c62
debasiskar-devel-007 <debasiskar007@gmail.com>
2015-08-31 18:09:03 +0530

    8/31/2015 4.41 PM

--- app/src/main/java/com/demoapp/contact.java
+++ app/src/main/java/com/demoapp/contact.java
@@ -10,20 +10,35 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,6 +49,7 @@ public class contact extends Activity {
     private ContactAdapter contactadapter;
     ArrayList<ContactItem> contacts = new ArrayList<>();
     private ProgressDialog dialog;
+    private Button startimport;
 
 
     @Override
@@ -41,7 +57,14 @@ public class contact extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
         textDetail = (TextView) findViewById(R.id.textView1);
+        startimport = (Button) findViewById(R.id.startimport);
         contactlist = (ListView) findViewById(R.id.contactlistview);
+
+        dialog = ProgressDialog.show(contact.this,
+                "Import is in progress ..", "Please wait...", true);
+
+
+        startimport.performClick();
 
 
         //fileNameList = getFileListfromSDCard();
@@ -51,9 +74,18 @@ public class contact extends Activity {
     public void showcontacts(View view) {
 
 
-        readContacts();
-        dialog = ProgressDialog.show(contact.this,
-                "Import is in progress ..", "Please wait...", true);
+
+        Toast.makeText(getApplicationContext(), "Creating your contacts list .." ,
+                Toast.LENGTH_LONG).show();
+
+
+
+       // SystemClock.sleep(6000);
+
+
+        //readContacts();
+
+        new ImageUploadTask().execute();
 
         //contactadapter = new ContactAdapter(this, R.layout.contact_list_item, readContacts());
         //contactlist.setAdapter(contactadapter);
@@ -135,6 +167,110 @@ public class contact extends Activity {
         return contacts;
     }
 
+
+
+
+    class ImageUploadTask extends AsyncTask<Void, Void, String> {
+        @SuppressWarnings("unused")
+        @Override
+        protected String doInBackground(Void... unsued) {
+
+
+            //StringBuffer sb = new StringBuffer();
+            //sb.append("......Contact Details.....");
+            ContentResolver cr = getContentResolver();
+            Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+            String phone = null;
+            String emailContact = null;
+            String emailType = null;
+            String image_uri = "";
+            Bitmap bitmap = null;
+
+            if (cur.getCount() > 0) {
+                while (cur.moveToNext()) {
+                    String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                    String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    //image_uri = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI));
+                    if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                        //System.out.println("name : " + name + ", ID : " + id);
+                        //sb.append("\n Contact Name:" + name);
+                        Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
+                        while (pCur.moveToNext()) {
+                            phone = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                            //sb.append("\n Phone number:" + phone);
+                       /* Toast.makeText(getApplicationContext(), "phone=" + phone + "name=" + name + "email=" + emailContact,
+                                Toast.LENGTH_LONG).show();*/
+                            //System.out.println("phone" + phone);
+                        }
+                        pCur.close();
+                        Cursor emailCur = cr.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?", new String[]{id}, null);
+                        while (emailCur.moveToNext()) {
+                            emailContact = emailCur.getString(emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                            emailType = emailCur.getString(emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.TYPE));
+                            //sb.append("\nEmail:" + emailContact + "Email type:" + emailType);
+                            //System.out.println("Email " + emailContact + " Email Type : " + emailType);
+                       /* Toast.makeText(getApplicationContext(), "phone=" + phone + "name=" + name + "email=" + emailContact,
+                                Toast.LENGTH_LONG).show();*/
+
+                            contacts.add(new ContactItem(emailContact,  name));
+
+
+                        }
+                        emailCur.close();
+
+                    }
+                    if (image_uri != null) {
+                        //System.out.println(Uri.parse(image_uri));
+                    }
+                /*bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),
+                        Uri.parse(image_uri));*/
+                    //sb.append("\n Image in Bitmap:" + bitmap);
+                    // System.out.println(bitmap);
+                }
+
+
+            }
+
+            try {
+                if (dialog.isShowing())
+                    dialog.dismiss();
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(),
+                        Toast.LENGTH_LONG).show();
+                Log.e(e.getClass().getName(), e.getMessage(), e);
+            }
+
+            Context context = contact.this;
+            Intent cameraintent = new Intent(context, MainActivity.class);
+
+
+            // Launch default browser
+            context.startActivity(cameraintent);
+
+            //return contacts;
+
+            return "Success";
+            // (null);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... unused) {
+
+        }
+
+        @Override
+        protected void onPostExecute(String sResponse) {
+            try {
+                if (dialog.isShowing())
+                    dialog.dismiss();
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(),
+                        Toast.LENGTH_LONG).show();
+                Log.e(e.getClass().getName(), e.getMessage(), e);
+            }
+        }
+
+    }
 
 
 
